5e4c6036ee02237b44c43fe20a33c244cb85f663
Rohit Nirmal <117462445+rohitnirmal10@users.noreply.github.com>
2023-06-09 23:37:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import plotly.graph_objs as go
 
 # Initialize the app
 app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+server = app.server
 
 # Alpha Vantage API key
 api_key = '4KFCDC6TZQ5KGR82'
